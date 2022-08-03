598f5e8ff8c52fd754722591251e8a9ba553212e
Lincoln-Andrade-Silva <lincolnandradesilva@outlook.com>
2022-08-03 19:55:52 -0300

    Initial commit

---
+++ src/main/java/com/api/application/ApiServiceApplication.java
@@ -0,0 +1,11 @@
+package com.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApplication.class, args);
+    }
+}
